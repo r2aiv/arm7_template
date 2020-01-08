43f7f1b14ed99cfabf2385d377c8daebb4a3a04c
Catherine1147 <catherine1147@yandex.ru>
2020-01-09 02:33:35 +0300

    'Initial'

---
+++ SAM7.s
@@ -0,0 +1,431 @@
+;/*****************************************************************************/
+;/* SAM7.S: Startup file for Atmel AT91SAM7 device series                     */
+;/*****************************************************************************/
+;/* <<< Use Configuration Wizard in Context Menu >>>                          */ 
+;/*****************************************************************************/
+;/* This file is part of the uVision/ARM development tools.                   */
+;/* Copyright (c) 2005-2006 Keil Software. All rights reserved.               */
+;/* This software may only be used under the terms of a valid, current,       */
+;/* end user licence from KEIL for a compatible version of KEIL software      */
+;/* development tools. Nothing else gives you the right to use this software. */
+;/*****************************************************************************/
+
+
+;/*
+; *  The SAM7.S code is executed after CPU Reset. This file may be 
+; *  translated with the following SET symbols. In uVision these SET 
+; *  symbols are entered under Options - ASM - Define.
+; *
+; *  REMAP: when set the startup code remaps exception vectors from
+; *  on-chip RAM to address 0.
+; *
+; *  RAM_INTVEC: when set the startup code copies exception vectors 
+; *  from on-chip Flash to on-chip RAM.
+; */
+
+
+				GET defines.s
+				
+                AREA    STACK, NOINIT, READWRITE, ALIGN=3
+
+Stack_Mem       SPACE   USR_Stack_Size
+__initial_sp    SPACE   ISR_Stack_Size
+Stack_Top
+
+Task1_Stack_Mem	SPACE	0x00000100
+Tasl1_Stack_Top
+
+Task2_Stack_Mem	SPACE	0x00000100
+Tasl2_Stack_Top
+
+Task3_Stack_Mem	SPACE	0x00000100
+Tasl3_Stack_Top
+
+
+                AREA    HEAP, NOINIT, READWRITE, ALIGN=3
+__heap_base
+Heap_Mem        SPACE   Heap_Size
+__heap_limit
+
+
+                PRESERVE8
+                
+				
+				AREA    DATA, DATA, READONLY, ALIGN=3
+Msg         DCB     " It fucking works! I'm so fucking happy! ", 10, 13, 0
+                PRESERVE8
+
+
+; Area Definition and Entry Point
+;  Startup Code must be linked first at Address at which it expects to run.
+
+                AREA    RESET, CODE, READONLY
+                ARM
+
+
+; Exception Vectors
+;  Mapped to Address 0.
+;  Absolute addressing mode must be used.
+;  Dummy Handlers are implemented as infinite loops which can be modified.
+
+Vectors         LDR     PC,Reset_Addr         
+                LDR     PC,Undef_Addr
+                LDR     PC,SWI_Addr
+                LDR     PC,PAbt_Addr
+                LDR     PC,DAbt_Addr
+                NOP                            ; Reserved Vector
+;               LDR     PC,IRQ_Addr
+                LDR     PC,[PC,#-0xF20]        ; Vector From AIC_IVR
+;               LDR     PC,FIQ_Addr
+                LDR     PC,[PC,#-0xF20]        ; Vector From AIC_FVR
+
+Reset_Addr      DCD     Reset_Handler
+Undef_Addr      DCD     Undef_Handler
+SWI_Addr        DCD     SWI_Handler
+PAbt_Addr       DCD     PAbt_Handler
+DAbt_Addr       DCD     DAbt_Handler
+                DCD     0                      ; Reserved Address
+IRQ_Addr        DCD     IRQ_Handler
+FIQ_Addr        DCD     FIQ_Handler
+
+Undef_Handler   B       Undef_Handler
+SWI_Handler     B       SWI_Handler
+PAbt_Handler    B       PAbt_Handler
+DAbt_Handler    B       DAbt_Handler
+IRQ_Handler     B       IRQ_Handler
+FIQ_Handler     B       FIQ_Handler
+
+
+; Reset Handler
+
+                EXPORT  Reset_Handler
+Reset_Handler   
+
+
+; Setup RSTC
+                IF      RSTC_SETUP != 0
+                LDR     R0, =RSTC_BASE
+                LDR     R1, =RSTC_MR_Val
+                STR     R1, [R0, #RSTC_MR]
+                ENDIF
+
+
+; Setup EFC0
+                IF      EFC0_SETUP != 0
+                LDR     R0, =EFC_BASE
+                LDR     R1, =EFC0_FMR_Val
+                STR     R1, [R0, #EFC0_FMR]
+                ENDIF
+
+; Setup EFC1
+                IF      EFC1_SETUP != 0
+                LDR     R0, =EFC_BASE
+                LDR     R1, =EFC1_FMR_Val
+                STR     R1, [R0, #EFC1_FMR]
+                ENDIF
+
+; Setup WDT
+                IF      WDT_SETUP != 0
+                LDR     R0, =WDT_BASE
+                LDR     R1, =WDT_MR_Val
+                STR     R1, [R0, #WDT_MR]
+                ENDIF
+
+
+; Setup PMC
+                IF      PMC_SETUP != 0
+                LDR     R0, =PMC_BASE
+
+;  Setup Main Oscillator
+                LDR     R1, =PMC_MOR_Val
+                STR     R1, [R0, #PMC_MOR]
+
+;  Wait until Main Oscillator is stablilized
+                IF      (PMC_MOR_Val:AND:PMC_MOSCEN) != 0
+MOSCS_Loop      LDR     R2, [R0, #PMC_SR]
+                ANDS    R2, R2, #PMC_MOSCS
+                BEQ     MOSCS_Loop
+                ENDIF
+
+;  Setup the PLL
+                IF      (PMC_PLLR_Val:AND:PMC_MUL) != 0
+                LDR     R1, =PMC_PLLR_Val
+                STR     R1, [R0, #PMC_PLLR]
+
+;  Wait until PLL is stabilized
+PLL_Loop        LDR     R2, [R0, #PMC_SR]
+                ANDS    R2, R2, #PMC_LOCK
+                BEQ     PLL_Loop
+                ENDIF
+
+;  Select Clock
+                IF      (PMC_MCKR_Val:AND:PMC_CSS) == 1     ; Main Clock Selected
+                LDR     R1, =PMC_MCKR_Val
+                AND     R1, #PMC_CSS
+                STR     R1, [R0, #PMC_MCKR]
+WAIT_Rdy1       LDR     R2, [R0, #PMC_SR]
+                ANDS    R2, R2, #PMC_MCKRDY
+                BEQ     WAIT_Rdy1
+                LDR     R1, =PMC_MCKR_Val
+                STR     R1, [R0, #PMC_MCKR]
+WAIT_Rdy2       LDR     R2, [R0, #PMC_SR]
+                ANDS    R2, R2, #PMC_MCKRDY
+                BEQ     WAIT_Rdy2
+                ELIF    (PMC_MCKR_Val:AND:PMC_CSS) == 3     ; PLL  Clock Selected
+                LDR     R1, =PMC_MCKR_Val
+                AND     R1, #PMC_PRES
+                STR     R1, [R0, #PMC_MCKR]
+WAIT_Rdy1       LDR     R2, [R0, #PMC_SR]
+                ANDS    R2, R2, #PMC_MCKRDY
+                BEQ     WAIT_Rdy1
+                LDR     R1, =PMC_MCKR_Val
+                STR     R1, [R0, #PMC_MCKR]
+WAIT_Rdy2       LDR     R2, [R0, #PMC_SR]
+                ANDS    R2, R2, #PMC_MCKRDY
+                BEQ     WAIT_Rdy2
+                ENDIF   ; Select Clock
+                ENDIF   ; PMC_SETUP
+
+
+; Copy Exception Vectors to Internal RAM
+
+                IF      :DEF:RAM_INTVEC
+                ADR     R8, Vectors         ; Source
+                LDR     R9, =RAM_BASE       ; Destination
+                LDMIA   R8!, {R0-R7}        ; Load Vectors 
+                STMIA   R9!, {R0-R7}        ; Store Vectors 
+                LDMIA   R8!, {R0-R7}        ; Load Handler Addresses 
+                STMIA   R9!, {R0-R7}        ; Store Handler Addresses
+                ENDIF
+
+
+; Remap on-chip RAM to address 0
+
+MC_BASE EQU     0xFFFFFF00      ; MC Base Address
+MC_RCR  EQU     0x00            ; MC_RCR Offset
+
+                IF      :DEF:REMAP
+                LDR     R0, =MC_BASE
+                MOV     R1, #1
+                STR     R1, [R0, #MC_RCR]   ; Remap
+                ENDIF
+
+
+; Setup Stack for each mode
+
+                LDR     R0, =Stack_Top
+
+;  Enter Undefined Instruction Mode and set its Stack Pointer
+                MSR     CPSR_c, #Mode_UND:OR:I_Bit:OR:F_Bit
+                MOV     SP, R0
+                SUB     R0, R0, #UND_Stack_Size
+
+;  Enter Abort Mode and set its Stack Pointer
+                MSR     CPSR_c, #Mode_ABT:OR:I_Bit:OR:F_Bit
+                MOV     SP, R0
+                SUB     R0, R0, #ABT_Stack_Size
+
+;  Enter FIQ Mode and set its Stack Pointer
+                MSR     CPSR_c, #Mode_FIQ:OR:I_Bit:OR:F_Bit
+                MOV     SP, R0
+                SUB     R0, R0, #FIQ_Stack_Size
+
+;  Enter IRQ Mode and set its Stack Pointer
+                MSR     CPSR_c, #Mode_IRQ:OR:I_Bit:OR:F_Bit
+                MOV     SP, R0
+                SUB     R0, R0, #IRQ_Stack_Size
+
+;  Enter Supervisor Mode and set its Stack Pointer
+                MSR     CPSR_c, #Mode_SVC:OR:I_Bit:OR:F_Bit
+                MOV     SP, R0
+                SUB     R0, R0, #SVC_Stack_Size
+
+;  Enter User Mode and set its Stack Pointer
+                MSR     CPSR_c, #Mode_USR
+                IF      :DEF:__MICROLIB
+
+                EXPORT __initial_sp
+
+                ELSE
+
+                MOV     SP, R0
+                SUB     SL, SP, #USR_Stack_Size
+
+                ENDIF
+
+
+; ----------------------ТОЧКА ВХОДА ЗДЕСЬ!!!-----------------------
+				
+ENTRYPOINT      
+
+; Включение тактирования PIOA
+				LDR		R0, =PMC_BASE
+				MOV 	R1, #1
+				LSL 	R1, #2
+				STR 	R1, [R0, #PMC_PCER]
+
+; Включение тактирования PIOB
+				LDR     R0, =PMC_BASE
+                MOV     R1, #1
+				LSL		R1, #3
+                STR     R1, [R0, #PMC_PCER]
+
+; PA0 - USART0_RX, PA1 - USART0_TX
+				LDR 	R0, =PIOA_BASE
+				MOV 	R1, #3				
+				STR		R1, [R0, #PIO_ASR]
+				STR		R1, [R0, #PIO_PDR]
+				STR		R1, [R0, #PIO_ODR]
+				STR		R1, [R0, #PIO_IFDR]
+				STR		R1, [R0, #PIO_CODR]
+				
+; Включение PB20 на выход
+				LDR R0, =PIOB_BASE
+				MOV R1, #1
+				LSL R1, #20
+				STR R1, [R0, #PIO_OER]
+				
+; Тактирование USART0
+				LDR     R0, =PMC_BASE
+                MOV     R1, #1
+				LSL		R1, #6
+                STR     R1, [R0, #PMC_PCER]
+
+
+; Настройка USART0
+
+				LDR		R0, =USART0_BASE
+				MOV 	R1, #0xAC ; Сброс и запрет
+				STR		R1, [R0, #USART_CR]
+
+				LDR		R0, =USART0_BASE
+				MOV     R1, #0xFFFFFFFF
+				STR 	R1, [R0, #USART_IDR]
+
+				LDR		R0, =USART0_BASE
+				MOV 	R1, #3
+				LSL		R1, #6
+				MOV		R2, #4
+				LSL		R2, #9
+				ORR 	R1, R2
+				STR		R1, [R0, #USART_MR]
+				
+				; 115200
+				LDR 	R0, =USART0_BASE
+				MOV 	R1, #26				
+				STR 	R1, [R0, #USART_BRGR]
+				
+				LDR 	R0, =USART0_BASE
+				MOV 	R1, #5
+				LSL 	R1, #4
+				STR		R1, [R0, #USART_CR]
+				
+FOREVER
+
+;; Включение подсветки LCD
+				LDR R0, =PIOB_BASE
+				MOV R1, #1
+				LSL R1, #20
+				STR R1, [R0, #PIO_SODR]
+												
+				BL BLINKDELAY
+				
+;; Выключение подсветки LCD
+				LDR R0, =PIOB_BASE
+				MOV R1, #1
+				LSL R1, #20
+				STR R1, [R0, #PIO_CODR]
+				
+				;MOV R2, #'A'
+				;BL UART_SEND_CHR
+				LDR R0, =Msg
+				BL UART_SEND_STR
+				
+				BL BLINKDELAY
+
+				B FOREVER
+				
+BLINKDELAY		PROC
+	
+				PUSH {LR}
+				PUSH {R0}
+				PUSH {R1}
+		
+				MOV R0, #100	
+				LSL R0, #8
+DELAY_1				
+				SUB R0, #1
+				MOV R1, #200
+				
+DELAY_2			
+				SUB R1, #1
+				CMP R1, #0
+				BNE DELAY_2
+				
+				CMP R0,#0
+				BNE DELAY_1
+				
+				POP {R1}
+				POP {R0}
+				POP {PC}
+	
+				ENDP
+	
+; Отправка символа по UART
+; ВХОД: R2 - отправляемый символ
+UART_SEND_CHR	PROC
+				
+				PUSH {LR}
+				PUSH {R0}
+				PUSH {R1}
+				PUSH {R2}
+				
+				LDR R0, =USART0_BASE
+				MOV R1, R2
+				STR R1, [R0, #USART_THR]
+				
+; Дожидаемся окончания отправки
+
+WAIT_SENT
+				EOR R0, R0
+				ADD R0, #USART0_BASE
+				ADD R0, #USART_CSR
+				LDR R1, [R0]
+				MOV R2, #1
+				LSL R2, #9
+				TST R1, R2 ; бит "передача завершена"
+				BEQ WAIT_SENT
+				
+				POP {R2}
+				POP {R1}
+				POP {R0}
+				POP {PC}
+				
+				ENDP
+
+; Отправка строки по UART
+; ВХОД: R0 - адрес строки
+UART_SEND_STR	PROC
+
+                PUSH    {LR}
+                PUSH    {R0}
+                PUSH    {R1}
+				PUSH    {R2}
+				
+				
+NEXT_CHR
+				LDRB 	R2, [R0]
+				CMP 	R2, #0
+				BEQ 	STR_END
+				BL 		UART_SEND_CHR
+				ADD 	R0, #1
+				B 		NEXT_CHR						
+STR_END
+				POP     {R2}
+                POP     {R1}
+                POP     {R0}
+                POP     {PC}
+                ENDP 
+
+
+                END
---
+++ defines.s
@@ -0,0 +1,128 @@
+; Standard definitions of Mode bits and Interrupt (I & F) flags in PSRs
+
+Mode_USR        EQU     0x10
+Mode_FIQ        EQU     0x11
+Mode_IRQ        EQU     0x12
+Mode_SVC        EQU     0x13
+Mode_ABT        EQU     0x17
+Mode_UND        EQU     0x1B
+Mode_SYS        EQU     0x1F
+
+I_Bit           EQU     0x80            ; when I bit is set, IRQ is disabled
+F_Bit           EQU     0x40            ; when F bit is set, FIQ is disabled
+
+
+; Internal Memory Base Addresses
+FLASH_BASE      EQU     0x00100000   
+RAM_BASE        EQU     0x00200000
+
+UND_Stack_Size  EQU     0x00000000
+SVC_Stack_Size  EQU     0x00000008
+ABT_Stack_Size  EQU     0x00000000
+FIQ_Stack_Size  EQU     0x00000000
+IRQ_Stack_Size  EQU     0x00000080
+USR_Stack_Size  EQU     0x00000400
+
+ISR_Stack_Size  EQU     (UND_Stack_Size + SVC_Stack_Size + ABT_Stack_Size + \
+                         FIQ_Stack_Size + IRQ_Stack_Size)
+					
+Heap_Size       EQU     0x00000000
+	
+	
+; Reset Controller (RSTC) definitions
+RSTC_BASE       EQU     0xFFFFFD00      ; RSTC Base Address
+RSTC_MR         EQU     0x08            ; RSTC_MR Offset
+	
+
+RSTC_SETUP      EQU     1
+RSTC_MR_Val     EQU     0xA5000401
+
+
+; Embedded Flash Controller (EFC) definitions
+EFC_BASE        EQU     0xFFFFFF00      ; EFC Base Address
+EFC0_FMR        EQU     0x60            ; EFC0_FMR Offset
+EFC1_FMR        EQU     0x70            ; EFC1_FMR Offset
+	
+EFC0_SETUP      EQU     1
+EFC0_FMR_Val    EQU     0x00320100
+	
+EFC1_SETUP      EQU     0
+EFC1_FMR_Val    EQU     0x00320100
+
+
+; Watchdog Timer (WDT) definitions
+WDT_BASE        EQU     0xFFFFFD40      ; WDT Base Address
+WDT_MR          EQU     0x04            ; WDT_MR Offset
+WDT_SETUP       EQU     1
+WDT_MR_Val      EQU     0x00008000
+
+; Power Mangement Controller (PMC) definitions
+PMC_BASE        EQU     0xFFFFFC00      ; PMC Base Address
+PMC_PCER		EQU		0x10
+PMC_MOR         EQU     0x20            ; PMC_MOR Offset
+PMC_MCFR        EQU     0x24            ; PMC_MCFR Offset
+PMC_PLLR        EQU     0x2C            ; PMC_PLLR Offset
+PMC_MCKR        EQU     0x30            ; PMC_MCKR Offset
+PMC_SR          EQU     0x68            ; PMC_SR Offset
+PMC_MOSCEN      EQU     (1<<0)          ; Main Oscillator Enable
+PMC_OSCBYPASS   EQU     (1<<1)          ; Main Oscillator Bypass
+PMC_OSCOUNT     EQU     (0xFF<<8)       ; Main OScillator Start-up Time
+PMC_DIV         EQU     (0xFF<<0)       ; PLL Divider
+PMC_PLLCOUNT    EQU     (0x3F<<8)       ; PLL Lock Counter
+PMC_OUT         EQU     (0x03<<14)      ; PLL Clock Frequency Range
+PMC_MUL         EQU     (0x7FF<<16)     ; PLL Multiplier
+PMC_USBDIV      EQU     (0x03<<28)      ; USB Clock Divider
+PMC_CSS         EQU     (3<<0)          ; Clock Source Selection
+PMC_PRES        EQU     (7<<2)          ; Prescaler Selection
+PMC_MOSCS       EQU     (1<<0)          ; Main Oscillator Stable
+PMC_LOCK        EQU     (1<<2)          ; PLL Lock Status
+PMC_MCKRDY      EQU     (1<<3)          ; Master Clock Status
+PMC_SETUP       EQU     1
+PMC_MOR_Val     EQU     0x00000601
+PMC_PLLR_Val    EQU     0x00191C05
+PMC_MCKR_Val    EQU     0x00000007
+	
+
+; Адреса периферийных устройств
+PIOA_BASE		EQU		0xFFFFF400 ; Порт "А"
+PIOB_BASE		EQU		0xFFFFF600 ; Порт "В"
+DBGU_BASE		EQU		0xFFFFF200 ; Отладочный UART
+
+; Смещения регистров периферийных устройств
+
+; Порты ввода-выводы
+PIO_PDR			EQU		0x0004
+PIO_OER			EQU     0x0010
+PIO_ODR			EQU		0x0014
+PIO_IFDR		EQU		0x0024
+PIO_SODR	    EQU 	0x0030
+PIO_CODR		EQU		0x0034
+PIO_ASR			EQU		0x0070
+PIO_BSR			EQU		0x0074
+	
+; Отладочный UART
+
+DBGU_CR			EQU		0x00000000
+DBGU_MR			EQU		0x00000004
+DBGU_CSR		EQU		0x00000014
+DBGU_THR		EQU		0x0000001C
+DBGU_IDR		EQU		0x0000000C
+DBGU_BRGR		EQU		0x00000020
+	
+; DMA для отладочного UART'a
+
+DBGU_PDC_BASE	EQU		0xFFFFF300
+DBGU_PDC_PTCR	EQU		0x00000020
+	
+; USART0
+
+USART0_BASE 	EQU		0xFFFC0000
+USART_CR		EQU		0x0000
+USART_MR		EQU		0x0004
+USART_CSR		EQU		0x0014
+USART_THR		EQU		0x001C
+USART_BRGR		EQU		0x0020
+USART_IDR		EQU		0x000C
+
+						 
+				END
